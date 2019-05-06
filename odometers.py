03cd2f6068a16f1fa7f5a50396c8d91fbdd1ea39
jyothisreekontheti <50200545+jyothisreekontheti@users.noreply.github.com>
2019-05-06 14:52:01 +0530

    Update odometers.py

--- odometers.py
+++ odometers.py
@@ -19,11 +19,35 @@ def next_num(num,l1):
         a = l1.index(int(num))
         return l1[a + 1]
 
-def next_nth_reading(num,n):
-    if num == l1[-1]:
+def previous_nth_num(num,l1,n):
+    for i in range(n):
+        a = l1.index(int(num))
+        p = previous(num,l1)
+        num = l1[a - 1]
+    return num
 
+def next_nth_num(num,l1,m):
+    for i in range(m):
+        a = l1.index(int(num))
+        next_nth = next_num(num,l1)
+        if num == l1[-1]:
+            num = l1[0]
+        else:
+            num = l1[a + 1]
+    return num
 
-
+def difference(num1,num2,l1):
+    if num2 > num1:
+        return l1.index(num2) - l1.index(num1)
+    else:
+        count = 0
+        while num1 != num2:
+            count,a = count + 1,l1.index(num1)
+            if num1 = l1[-1]:
+                num1 = l1[0]
+            else:
+                num = l1[a + 1]
+                    
 for x in range(start,limit + 1):
     if list(str(x)) == sorted(str(x)) and x % 11 != 0 and isAscending(str(x)):
         l1.append(x)
@@ -31,5 +55,13 @@ for x in range(start,limit + 1):
 if num not in l1:
     print("Invalid")
 else:
-    print(previous(num,l1))
-    print(next_num(num,l1))
+    print("previous number = ",previous(num,l1))
+    print("next number = ",next_num(num,l1))
+    
+    n,m = int(input()),int(input())
+    print("previous nth number = ",previous_nth_num(num,l1,n))
+    print("next nth number = ",next_nth_num(num,l1,m))
+    
+    num1,num2 = int(input()),int(input())
+    print("difference = ",difference(num1,num2,l1))
+    
